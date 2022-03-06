60d3fa76d34f62dd20988b65dbf5b9e847b627cf
Tim La <tlamarketing111@gmail.com>
2022-03-06 11:31:13 -0500

    able to display list of movies from search input field

--- src/App.js
+++ src/App.js
@@ -7,16 +7,17 @@ import SearchIcon from './search.svg';
 // const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=9b9f494f";
 const API_URL = "http://www.omdbapi.com?apikey=b6003d8a";
 
-const movie1 = {
-  "Title": "Amazing Spiderman Syndrome",
-  "Year": "2012",
-  "imdbID": "tt2586634",
-  "Type": "movie",
-  "Poster": "N/A"
-}
+// const movie1 = {
+//   "Title": "Amazing Spiderman Syndrome",
+//   "Year": "2012",
+//   "imdbID": "tt2586634",
+//   "Type": "movie",
+//   "Poster": "N/A"
+// }
 
 const App = () => {
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
@@ -36,29 +37,27 @@ const App = () => {
       <div className="search">
         <input
           placeHolder="Search for movies"
-          value="Superman"
-          onChange={() => { }}
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
-        <image
+        <img
           src={SearchIcon}
           alt="search"
-          onClick={() => { }}
+          onClick={() => searchMovies(searchTerm)}
         />
       </div>
 
-      {
-        movies?.length > 0 ? (
-          <div className="container">
-            {movies.map((movie) => (
-              <MovieCard movie={movie} />
-            ))}
-          </div>
-        ) : (
-          <div className="empty">
-            <h2>No movies found</h2>
-          </div>
-        )
-      }
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
 
 
     </div>

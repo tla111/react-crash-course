197d695d05e4906fdf2eca7050f90ebc2171f9a3
Tim La <tlamarketing111@gmail.com>
2022-03-06 11:03:25 -0500

    able to fetch movie by name of title

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import { useEffect } from 'react';
 import './App.css'
-import Counter from './Counter.js'
+
+// const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=9b9f494f";
+const API_URL = "http://www.omdbapi.com?apikey=b6003d8a";
 
 const App = () => {
 
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data);
+  }
+
+  useEffect(() => {
+    searchMovies("Spiderman");
+  }, []);
+
   return (
     <div className="App">
-      <Counter />
+      <h1>App</h1>
     </div>
   );
 }
--- src/Counter.js
+++ src/Counter.js
@@ -1,16 +1,12 @@
-import {useState, useEffect} from 'react'
+import { useState } from 'react'
 
 const Counter = () => {
     const [counter, setCounter] = useState(0);
 
-    useEffect(() => {
-     alert(counter);   
-    }, [counter])
-
     return (
         <>
             <button onClick={() => setCounter((prevCount) => prevCount - 1)}>Decrement</button>
-            <p style={{fontSize:"36px"}}>{counter}</p>
+            <p style={{ fontSize: "36px" }}>{counter}</p>
             <button onClick={() => setCounter((prevCount) => prevCount + 1)}>Increment</button>
         </>
     )

1225467867a1c902889e25d4f287d7660411f50a
Tim La <tlamarketing111@gmail.com>
2022-03-06 11:26:03 -0500

    able to dynamically display movies

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 import MovieCard from './MovieCard';
 
@@ -16,12 +16,13 @@ const movie1 = {
 }
 
 const App = () => {
+  const [movies, setMovies] = useState([]);
 
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
 
-    console.log(data);
+    setMovies(data.Search);
   }
 
   useEffect(() => {
@@ -45,9 +46,19 @@ const App = () => {
         />
       </div>
 
-      <div className="container">
-        <MovieCard movie1={movie1} />
-      </div>
+      {
+        movies?.length > 0 ? (
+          <div className="container">
+            {movies.map((movie) => (
+              <MovieCard movie={movie} />
+            ))}
+          </div>
+        ) : (
+          <div className="empty">
+            <h2>No movies found</h2>
+          </div>
+        )
+      }
 
 
     </div>
